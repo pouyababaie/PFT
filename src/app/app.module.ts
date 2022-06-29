68062fc44d6dc7d40c43f3c3d89c5d5d56cc93bf
pouyababaie <pouyababaie123@2gmail.com>
2022-06-29 19:50:19 +0430

    steps moved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploaderComponent } from '../steps/uploader/uploader.component';
+import { FormComponent } from '../steps/form/form.component';
+import { UserTableComponent } from '../steps/user-table/user-table.component';
+import { SummeryComponent } from '../steps/summery/summery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploaderComponent,
+    FormComponent,
+    UserTableComponent,
+    SummeryComponent
   ],
   imports: [
     BrowserModule,
